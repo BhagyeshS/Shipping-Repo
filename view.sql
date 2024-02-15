95088c85b04d9666935687107e0920281a9c3ec4
BhagyeshS <160107690+BhagyeshS@users.noreply.github.com>
2024-02-15 15:58:04 +0530

    Update view.sql

--- view.sql
+++ view.sql
@@ -2,13 +2,15 @@
 
 CREATE VIEW ShipmentDetails AS
 SELECT     
-C.CustomerName,     
-S.ImageURL AS ShipmentImage,     
-S.BidStartTime,     
-S.BidEndTime,     
-S.MaxBidAmountFROM     
-Shipments SJOIN Customers C ON S.CustomerID = C.CustomerID;
-
+    C.CustomerName,     
+    S.ImageURL AS ShipmentImage,     
+    S.BidStartTime,     
+    S.BidEndTime,     
+    S.MaxBidAmount
+FROM     
+    Shipments S
+JOIN 
+    Customers C ON S.CustomerID = C.CustomerID;
 
 
 select* from ShipmentDetails;
